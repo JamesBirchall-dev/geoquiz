43ce4fb7404be88b725e47fca63c2881acd93c7c
JamesBirchall-dev <jamesbdorel@gmail.com>
2025-08-03 15:46:26 +0100

    comment js sections clearly

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,22 @@
+// DOM Elements
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById ('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
-/* randomises questions whilst knowing index*/
+/* randomises questions whilst tracking index*/
 let shuffledQuestions, currentQuestionIndex
 
-
+//starts the game on click
 startButton.addEventListener('click', startGame)
+// next question 
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++
     setNextQuestion()
 })
 
+// function to start quiz
 function startGame() {
     startButton.classList.add('hide') // hides the start button after clicking
     shuffledQuestions = questions.sort(() => Math.random() - .5) // shuffles questions
@@ -22,26 +25,26 @@ function startGame() {
     setNextQuestion()
 }
 
-
+// move the next question upon clicking 
 function setNextQuestion() {
     resetState() /*resets question state*/
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
-
+// displays question
 function showQuestion(question) {
     questionElement.innerText = question.question
-    question.answers.forEach(answer => {
+    question.answers.forEach(answer => { //creates buttons for answers
         const button = document.createElement ('button')
         button.innerText = answer.text
         button.classList.add('btn')
-        if (answer.correct) {
+        if (answer.correct) { // marks the button when correct answer selected
             button.dataset.correct = answer.correct
         }
         button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
+        answerButtonsElement.appendChild(button) //adds to DOM
     })
 }
-
+// restart quiz function
 function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
@@ -49,7 +52,7 @@ function resetState() {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
-
+// sets the status correct or wrong
 function selectAnswer(e) {
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
@@ -73,12 +76,13 @@ function setStatusClass(element, correct) {
             element.classList.add('wrong')
         }
     }
-
+//clears the statuus from element
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
 
+// Question Array
 
 const questions = [
     {
